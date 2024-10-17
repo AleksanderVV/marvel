4285f8e38cf5a37ae535b2a01eee97e9d57d0b92
Aleksander Voronyuk <voronyuk.aleksander@gmail.com>
2024-10-17 14:26:11 +0300

    added pagination to the page

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -10,26 +10,48 @@ class CharList extends Component {
     state = {
         charList: [],
         loading: true,
-        error: false
+        error: false,
+        newItemLoading: false,
+        offset: 210,
+        charEnded: false
     }
 
     marvelService = new MarvelService();
 
     componentDidMount() {
-        this.updateList();
+        this.onRequest();
     }
 
-    updateList = () => {
+    onRequest = (offset) => {
+        this.onCharListLoading();
+
         this.marvelService
-        .getAllCharacters()
+        .getAllCharacters(offset)
         .then(this.onCharListLoaded)
+        .catch(this.onError)
     }
 
-    onCharListLoaded = (charList) => {
+    onCharListLoading = () => {
         this.setState({
-            charList,
-            loading: false
+            newItemLoading: true
         })
+    }
+
+    onCharListLoaded = (newCharList) => {
+        let ended = false;
+        if (newCharList.length < 9) {
+            ended = true;
+        }
+
+        this.setState(({offset, charList}) => (
+            {
+                charList: [...charList, ...newCharList],
+                loading: false,
+                newItemLoading: false,
+                offset: offset + 9,
+                charEnded: ended
+            }
+        ))
     }
 
     onError = () => {
@@ -66,7 +88,7 @@ class CharList extends Component {
 
 
     render() {
-        const {charList, loading, error} = this.state;
+        const {charList, loading, error, offset, newItemLoading, charEnded} = this.state;
         const items = this.renderItems(charList);
 
         const errorMessage = error ? <ErrorMessage/> : null;
@@ -75,13 +97,15 @@ class CharList extends Component {
 
         return (
             <div className="char__list">
-                <ul className="char__grid">
-                    {errorMessage}
-                    {spinner}
-                    {content}
-                </ul>
-                <button className="button button__main button__long">
-                    <div className="inner">load more</div>
+                {errorMessage}
+                {spinner}
+                {content}
+                <button 
+                    className="button button__main button__long"
+                    disabled={newItemLoading}
+                    style={{'display': charEnded ? 'none' : 'block'}}
+                    onClick={() => this.onRequest(offset)}>
+                        <div className="inner">load more</div>
                 </button>
             </div>
         )
