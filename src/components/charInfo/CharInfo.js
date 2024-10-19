32047749898cfbd01c83d4b16e6c83f6d82991ac
Aleksander Voronyuk <voronyuk.aleksander@gmail.com>
2024-10-19 16:02:12 +0300

    changed class components to functional components using Hooks

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import MarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
@@ -6,72 +6,55 @@ import Skeleton from '../skeleton/Skeleton';
 
 import './charInfo.scss';
 
-class CharInfo extends Component {
-    state = {
-        char: null,
-        loading: false,
-        error: false
-    }
+const CharInfo = (props) => {
+    const [char, setChar] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
 
-    marvelService = new MarvelService();
+    const marvelService = new MarvelService();
 
-    componentDidMount() {
-        this.updateChar();
-    }
+    // eslint-disable-next-line
+    useEffect(() => updateChar(), [props.charId]); 
 
-    componentDidUpdate(prevProps) {
-        return (this.props.charId !== prevProps.charId) ? this.updateChar() : null;
-    }   
-
-    updateChar = () => {
-        const {charId} = this.props;
+    const updateChar = () => {
+        const {charId} = props;
         if (!charId) {return;}
 
-        this.onCharLoading();
+        onCharLoading();
 
-        this.marvelService
+        marvelService
             .getCharacter(charId)
-            .then(this.onCharLoaded)
-            .catch(this.onError);
+            .then(onCharLoaded)
+            .catch(onError);
     }
 
-    onCharLoaded = (char) => {
-        this.setState({
-            char,
-            loading: false
-        })
+    const onCharLoaded = (char) => {
+        setChar(char);
+        setLoading(false);
     }
 
-    onCharLoading = () => {
-        this.setState({
-            loading: true})
+    const onCharLoading = () => {
+        setLoading(true);
     }
 
-    onError = () => {
-        this.setState({
-            loading: false,
-            error: true
-        })
+    const onError = () => {
+        setLoading(false);
+        setError(true);
     }
 
+    const skeleton = char || loading || error ? null : <Skeleton/>;
+    const errorMessage = error ? <ErrorMessage/> : null;
+    const spinner = loading ? <Spinner/> : null;
+    const content = !(loading || error || !char) ? <View char={char}/> : null;
 
-    render() {
-        const {char, loading, error} = this.state;
-
-        const skeleton = char || loading || error ? null : <Skeleton/>;
-        const errorMessage = error ? <ErrorMessage/> : null;
-        const spinner = loading ? <Spinner/> : null;
-        const content = !(loading || error || !char) ? <View char={char}/> : null;
-
-        return (
-            <div className="char__info">
-                {skeleton}
-                {errorMessage}
-                {spinner}
-                {content}
-            </div>
-        )
-    }
+    return (
+        <div className="char__info">
+            {skeleton}
+            {errorMessage}
+            {spinner}
+            {content}
+        </div>
+    )
   
 }
 
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,5 +1,4 @@
 class MarvelService {
-
   _apiBase = "https://gateway.marvel.com:443/v1/public/";
   _apiKey = "apikey=d16d8868d4036e3d39fdc98f82ffe14b";
   _baseOffset = 210;
@@ -8,35 +7,33 @@ class MarvelService {
     let res = await fetch(url);
 
     if (!res.ok) {
-      throw new Error(`Could not fetch ${url}, status ${res.status}`);
+        throw new Error(`Could not fetch ${url}, status: ${res.status}`);
     }
 
     return await res.json();
-  }
+}
 
-  getAllCharacters = async (offset = this._baseOffset) => {
+getAllCharacters = async (offset = this._baseOffset) => {
     const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
-
     return res.data.results.map(this._transformCharacter);
-  }
+}
 
-  getCharacter = async (id) => {
+getCharacter = async (id) => {
     const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
-
     return this._transformCharacter(res.data.results[0]);
-  }
+}
 
-  _transformCharacter = (char) => {
+_transformCharacter = (char) => {
     return {
-      id: char.id,
-      name: char.name,
-      description: char.description,
-      thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
-      homepage: char.urls[0].url,
-      wiki: char.urls[1].url,
-      comics: char.comics.items
+        id: char.id,
+        name: char.name,
+        description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
+        thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+        homepage: char.urls[0].url,
+        wiki: char.urls[1].url,
+        comics: char.comics.items
     }
-  }
+}
 }
 
 export default MarvelService;
